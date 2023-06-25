4a00bd402f56961794b7da6d8531a840585b833c
Szymon Rutkowski <Szymon.rutkowski1@gmail.com>
2023-06-25 17:18:07 +0200

    binding events moved to a separate function

--- JS/script.js
+++ JS/script.js
@@ -29,21 +29,7 @@
         render();
     }
 
-    const render = () => {
-        let htmlString = "";
-        for (const task of tasks) {
-            htmlString += `
-    <li
-    ${task.done ? " style=\"text-decoration: line-through\"" : ""}
-    >
-    <button class="js-done"> zrobione? </button>
-    <button class="js-remove">usuń</button>
-    ${task.content}
-    </li>
-    `;
-        }
-        document.querySelector(".js-tasks").innerHTML = htmlString;
-
+    const bindEvents = () => {
         const removeButtons = document.querySelectorAll(".js-remove");
 
         removeButtons.forEach((removeButton, index) => {
@@ -59,6 +45,24 @@
                 toggleTaskDone(index);
             });
         });
+    }
+
+    const render = () => {
+        let htmlString = "";
+        for (const task of tasks) {
+            htmlString += `
+    <li
+    ${task.done ? " style=\"text-decoration: line-through\"" : ""}
+    >
+    <button class="js-done"> zrobione? </button>
+    <button class="js-remove">usuń</button>
+    ${task.content}
+    </li>
+    `;
+        }
+        document.querySelector(".js-tasks").innerHTML = htmlString;
+
+        bindEvents();
     };
 
     const onFormSubmit = (event) => {
