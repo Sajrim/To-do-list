2a56753968133c6c21ef52b91d9e9e2159bb0ff7
Szymon Rutkowski <Szymon.rutkowski1@gmail.com>
2023-07-09 04:23:06 +0200

    typo corrects and little changes in css

--- JS/script.js
+++ JS/script.js
@@ -42,21 +42,19 @@
 
         for (const task of tasks) {
             tasksListHTMLContent += `
-    <li
-    class="tasks__item js-task"
-    >
-    <button class="tasks__button tasks__button-toggleDone js-toggleDone"> 
-    ${task.done ? "✔️" : ""}
-    </button>
-    <span class="tasks__content${task.done ? " tasks__content--done" : ""}">
-    ${task.content}
-    </span>
-    <button class="tasks__button tasks__button-remove js-remove">
-    🗑️
-    </button>
-    </li>
-    `;
-     }
+            <li class="tasks__item js-task">
+            <button class="tasks__button tasks__button-toggleDone js-toggleDone"> 
+            ${task.done ? "✔️" : ""}
+             </button>
+             <span class="tasks__content${ task.done ? " tasks__content--done" : ""}">
+             ${task.content}
+            </span>
+             <button class="tasks__button tasks__button-remove js-remove">
+            🗑️
+             </button>
+             </li>
+            `;
+              }
 
         document.querySelector(".js-tasks").innerHTML = tasksListHTMLContent;
 
