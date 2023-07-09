348994c5d0b474ccbcf797e42ae554d85cf52551
Szymon Rutkowski <Szymon.rutkowski1@gmail.com>
2023-07-09 19:07:17 +0200

    fixing line-through on toggleDone tasks

--- JS/script.js
+++ JS/script.js
@@ -46,7 +46,7 @@
             <button class="tasks__button tasks__button--toggleDone js-toggleDone"> 
             ${task.done ? "✔️" : ""}
              </button>
-             <span class="tasks__content${ task.done ? "tasks__content--done" : ""}">
+             <span class="tasks__content${ task.done ? " tasks__content--done" : ""}">
              ${task.content}</span>
              <button class="tasks__button tasks__button--remove js-remove">
             🗑️
